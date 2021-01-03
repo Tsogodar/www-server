ddec315daef3bcdb4b0b69448905ce4d2775573a
Tsogodar <marcin.gryczon@gmail.com>
2021-01-04 00:02:05 +0100

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require( 'path' );
+const cors = require( 'cors' );
 const express = require( 'express' );
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(cors());
 app.get( '*', express.static( path.join( __dirname, 'dist/' ) ) );
 
 app.listen( port, () => console.log( `Server running on port ${ port }` ) );
